e54f4eb5d61a8933211e6f54998196a1d01e4648
Gaba <gabrielfpaula@gmail.com>
2026-05-05 08:43:21 -0300

    add missing images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
