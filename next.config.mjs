ef4cf68c62a2d40cc90d0e173fa8bd42f72f6269
Gaba <gabrielfpaula@gmail.com>
2026-07-16 18:07:13 -0300

    Fix posthog

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  env: {
+    NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
+    NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  },
   images: {
     unoptimized: true,
   },
